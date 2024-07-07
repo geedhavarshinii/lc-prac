a8318a2f8c82591a673aaa10ab7f91f230157c35
Geedhavarshini Balaji <134434224+geedhavarshinii@users.noreply.github.com>
2024-07-07 12:40:56 +0530

    Time: 0 ms (100%), Space: 44.2 MB (57.98%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -17,18 +17,30 @@ class Solution {
     public Node copyRandomList(Node head) {
         if (head==null) return null;
 
-        HashMap<Node, Node> oldToNew = new HashMap<>();
         Node curr = head;
         while (curr!=null){
-            oldToNew.put(curr, new Node(curr.val));
-            curr = curr.next;
+            Node new_node = new Node(curr.val, curr.next);
+            curr.next = new_node;
+            curr = new_node.next;
         }
         curr = head;
         while (curr!=null){
-            oldToNew.get(curr).next = oldToNew.get(curr.next);
-            oldToNew.get(curr).random = oldToNew.get(curr.random);
-            curr = curr.next;
+            if (curr.random != null)
+            curr.next.random = curr.random.next;
+        curr = curr.next != null ? curr.next.next : null;
         }
-        return oldToNew.get(head);
+    
+    Node old_head = head;
+    Node new_head = head.next;
+    Node curr_old = old_head;
+    Node curr_new = new_head;
+    while (curr_old != null){
+        curr_old.next = curr_old.next.next;
+        curr_new.next = curr_new.next != null ? curr_new.next.next : null;
+        curr_old = curr_old.next;
+        curr_new = curr_new.next;
+    }
+    return new_head;
+
     }
 }
